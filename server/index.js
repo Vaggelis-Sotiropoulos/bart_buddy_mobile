a74eb1b45ce2e876ee137ac2a122a8a649e54b96
Vaggelis Sotiropoulos <vsotiropoulo@gmail.com>
2017-06-14 14:43:25 -0700

    Updates server index

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@
 const app = require('./app');
 const db = require('../db');
 const PORT = process.env.PORT || 1337;
-const twilio = require('./workers/twilio')
+const twilio = require('./workers/twilio');
+const trains = require('./routes/train_data.js');
+const update = require('./routes/update_schedule.js');
 
 app.listen(PORT, () => {
     console.log(`Bart Buddy listening on port ${PORT}!`);
